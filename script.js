202565e78b736b249dc2074a2c4f96cb227e86a6
Davi Roque <sowdavi@gmail.com>
2022-05-20 13:35:55 -0300

    correção_escrita_innerHTML

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ function calcular() {
 
     if (kg !== '' && m !== '') {
         var imc = (kg / (m * m)).toFixed(2)
-        resultado.innerHTML =`Seu imc é de ${imc}`
+        
         if (imc < 17) {
             resultado.style.color = '#009b47'
-            resultado.innerHTML =`O ${imc} está muito abaixo do peso recomendado.`
+            resultado.innerHTML =`O IMC de ${imc} está muito abaixo do peso recomendado.`
         }else if( imc >= 17 && imc <=18.49){
             resultado.style.color = '#8ac43f'
-            resultado.innerHTML =`O ${imc} está abaixo do peso recomendado.`
+            resultado.innerHTML =`O IMC de ${imc} está abaixo do peso recomendado.`
         }else if( imc >= 18.50 && imc < 25){
             resultado.style.color = '#b9d530'
-            resultado.innerHTML =`O ${imc} está ideal, esse é seu peso recomendado.`
+            resultado.innerHTML =`O IMC de ${imc} está ideal, esse é seu peso recomendado.`
         }else if( imc >= 25 && imc < 30){
             resultado.style.color = '#f9a11c'
-            resultado.innerHTML =`O ${imc} está acima do peso recomendado.`
+            resultado.innerHTML =`O IMC de ${imc} está acima do peso recomendado.`
         }else if( imc >=30 && imc < 35){
             resultado.style.color = '#f6951c'
-            resultado.innerHTML =`O ${imc} está indicando obesidade nivel 1.`
+            resultado.innerHTML =`O IMC de ${imc} está indicando obesidade nivel 1.`
         }else if (imc >= 35 && imc < 40){
             resultado.style.color = '#d1181f'
-            resultado.innerHTML =`O ${imc} está indicando obesidade nivel 2.`
+            resultado.innerHTML =`O IMC de ${imc} está indicando obesidade nivel 2.`
         }else if (imc >= 40){
             resultado.style.color = '#231f20'   
-            resultado.innerHTML =`O ${imc} está indicando obesidade nivel 3.` 
+            resultado.innerHTML =`O IMC de ${imc} está indicando obesidade nivel 3.` 
         }
     } else {
         alert('Preencha todos os campos')
